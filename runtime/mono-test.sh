b6a71c2bb14dda168ac396524f84dbec018fecb8
Martin Baulig <mabaul@microsoft.com>
2019-01-21 15:00:21 -0500

    Install certs as suggested by @akoeplinger.

--- runtime/mono-test.sh
+++ runtime/mono-test.sh
@@ -22,6 +22,14 @@ echo "--------------------------------------------------------------------------
 "${MONO_EXECUTABLE}" --version
 echo "---------------------------------------------------------------------------------------"
 
+if [ -x "/usr/bin/dpkg-architecture" ];
+	then
+	EXTRA_CONF_FLAGS="$EXTRA_CONF_FLAGS --host=`/usr/bin/dpkg-architecture -qDEB_HOST_GNU_TYPE`"
+	#force build arch = dpkg arch, sometimes misdetected
+	mkdir -p ~/.config/.mono/
+	wget -qO- https://download.mono-project.com/test/new-certs.tgz| tar zx -C ~/.config/.mono/
+fi
+
 if [ "$test_suite" = "--xunit" ]; then
     cd net_4_x || exit 1
     export MONO_PATH="$r/net_4_x/tests:$MONO_PATH"
